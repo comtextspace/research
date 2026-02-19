95d045e628370a8e0b12266da3da3b63fb497b78
Petro <petrovdrm86@yandex.ru>
2026-02-19 13:59:59 +0500

    Изменение Оглавления сайта и текстобзоров

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -75,7 +75,8 @@ export default {
     contributors: false,
     lastUpdatedText: "Последниее изменение",
     navbar: [
-      { text: "Обновления в TG", link: "https://t.me/pensadoj" },
+       { text: "Mastodon", link: "https://defcon.social/@pensadoj" },
+       { text: "Telegram", link: "https://t.me/pensadoj" }
     ],
   }),
 
